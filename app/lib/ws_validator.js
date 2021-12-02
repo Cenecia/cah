70c5b6b0ce59c1761468a0b01e6ab30051fa38f1
Maxwell French <maxwell@frenchnet.org>
2021-12-02 16:12:59 -0500

    Add more structure to ws_validator.

--- app/lib/ws_validator.js
+++ app/lib/ws_validator.js
@@ -1,52 +1,55 @@
 const joi = require("joi");
 
 const OBJECTID_LENGTH = 24; //twelve hexadecimal bytes
-/*const whiteCardSchema = new mongoose.Schema({
-  set: { type: ObjectId, required: true, ref: "Sets" },
-  text: { type: String },
-  blankCard: { type: Boolean, default: false }
-});
-*/
-const white_card_schema = joi.object({
-    set: joi.string()
-        .length(OBJECTID_LENGTH)
-        .required(),
-    text: joi.string()
-        .min(1)
-        .required(),
-    is_blank: joi.boolean()
-        .required()
-});
 
-const player_schema = joi.object({
-    name: joi.string()
-        .required(),
-    active: joi.boolean()
-        .required(),
-    points: joi.number()
-        .integer()
-        .min(0)
-        .required(),
-    mulligans: joi.number()
-        .integer()
-        .min(0)
-        .required(),
-    hand: joi.array(white_card_schema)
-        .required()
-});
+const ws_schema = {
+    validate: function(schema, data) {
+        schema.validate(data);
+    },
+
+    white_card: joi.object({
+        set: joi.string()
+            .length(OBJECTID_LENGTH)
+            .required(),
+        text: joi.string()
+            .min(1)
+            .required(),
+        is_blank: joi.boolean()
+            .required()
+    }),
+
+    player: joi.object({
+        name: joi.string()
+            .required(),
+        active: joi.boolean()
+            .required(),
+        points: joi.number()
+            .integer()
+            .min(0)
+            .required(),
+        mulligans: joi.number()
+            .integer()
+            .min(0)
+            .required(),
+        hand: joi.array(white_card_schema)
+            .required()
+    }),
 
 
-const join_request_schema = joi.object({
-    game_id: joi.string()
-        .length(OBJECTID_LENGTH)
-        .required()
-});
+    join_request: joi.object({
+        game_id: joi.string()
+            .length(OBJECTID_LENGTH)
+            .required()
+    }),
 
-const join_response_schema = join.object({
-    game_id: joi.string()
-        .length(OBJECTID_LENGTH)
-        .required(),
-    player_list: joi.array(player_schema)
-        .required()
-});
+    join_response: joi.object({
+        game_id: joi.string()
+            .length(OBJECTID_LENGTH)
+            .required(),
+        player_list: joi.array(player_schema)
+            .required()
+    })
 
+}
+
+module.exports = ws_schemas;
